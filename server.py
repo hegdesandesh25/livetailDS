667bcd24dec9e1d024cae8a43789c5dc388aba7e
hegdesandesh25 <root@sandesh-test>
2023-07-08 19:46:03 +0100

    fixes for server.py to serve Redis data

--- server.py
+++ server.py
@@ -1,28 +1,35 @@
-from flask import Flask, jsonify
 import redis
-import socketio
-from socket_io_emitter import Emitter
-from queue import Queue
-import atexit
 import time
-import eventlet.wsgi
+import atexit
+import socketio
 
-#setup connection pool
-constr = redis.ConnectionPool(host='localhost',port=6379,db=0)
+# Set up a connection pool
+pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
 
-# Connect to redis
-r= redis.Redis(connection_pool=constr)
+# Create a Redis connection
+r = redis.Redis(connection_pool=pool)
 
-#set no of log-streams to process
-Log_stream_size=100
+# Set a variable for batch size
+Log_stream_size = 100
 
 # Set up a Socket.IO server
-io = socketio.Server(cors_allowed_origins='*')
+io = socketio.Server(cors_allowed_origins='*', debug=True)
+app = socketio.WSGIApp(io,static_files={'/': {'content_type': 'text/html', 'filename': 'index.html'}})
 
-# Set up a Flask app and a Socket.IO server
-app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins='*')
+# Define event handlers
+@io.event
+def connect(sid, environ):
+    print('connect', sid)
 
+@io.event
+def my_message(sid, data):
+    print('message', data)
+
+@io.event
+def disconnect(sid):
+    print('disconnect', sid)
+
+'''
 def move_items():
     try:
         # Use XREAD to read from the stream
@@ -36,41 +43,35 @@ def move_items():
                     io.emit('new_item', item.decode('utf-8'))
     except redis.exceptions.RedisError as e:
         print(f"Error reading items from stream: {e}")
-
 '''
-
-#Function to move items from list to stream
+# Define a function to move items from list to stream and emit them
 def move_items():
     try:
         pipeline = r.pipeline()
-        items = r.lrange('logstash',0,Log_stream_size-1)
+        items = r.lrange('logstash', 0, Log_stream_size-1)
         if items:
             for item in items:
-                #write to stream 
-                pipeline.xadd('logstash-stream',{'message': item})
-                #print(item)
-                #Emit the item to all connected socket.io client
-                io.emit('new_item',item.decode('utf-8'))
-                
-            #remove processed item from list
-            pipeline.ltrim('logstash',len(items),-1)
+                # Write the item to the stream
+                pipeline.xadd('logstash-stream', {'message': item})
+                # Emit the item to all connected Socket.IO clients
+                io.emit('new_item', item.decode('utf-8'))
+            # Remove the processed items from the list
+            pipeline.ltrim('logstash', len(items), -1)
             pipeline.execute()
     except redis.exceptions.RedisError as e:
         print(f"Error moving items from list to stream: {e}")
 
-'''
+
 # Define a function to be called on exit
 def on_exit():
-    print(" shutting down...")
+    print("Gracefully shutting down...")
     move_items()  # Move any remaining items
 
 atexit.register(on_exit)
 
 # Start the Socket.IO server in a separate thread
-#import eventlet.wsgi
-#eventlet.spawn(eventlet.wsgi.server, eventlet.listen(('', 8000)), app)
-eventlet.spawn(eventlet.wsgi.server, eventlet.listen(('127.0.0.1', 8000)), app)
-
+import eventlet.wsgi
+eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 8000)), app)
 
 while True:
     move_items()
